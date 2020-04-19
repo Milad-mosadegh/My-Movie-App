e802e37a17b02f64b5866c064b2e147f425ee3b0
sdf <dfvsdfvadv>
2020-04-19 18:01:24 +0200

    Updates

---
+++ precache-manifest.1116bc3bfc399122a045d8b1bb793421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e73491492ffafaa774a9abdc168c3b0",
+    "url": "/My-Movie-App/index.html"
+  },
+  {
+    "revision": "874a90d2de2d053da3ed",
+    "url": "/My-Movie-App/static/css/main.b8402fbc.chunk.css"
+  },
+  {
+    "revision": "1d0639a1fd3e4e8b2a0e",
+    "url": "/My-Movie-App/static/js/2.eeedddbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Movie-App/static/js/2.eeedddbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874a90d2de2d053da3ed",
+    "url": "/My-Movie-App/static/js/main.10e8f48a.chunk.js"
+  },
+  {
+    "revision": "dd6fa0396dcc9bcd9556",
+    "url": "/My-Movie-App/static/js/runtime-main.9dd17a2a.js"
+  }
+]);
